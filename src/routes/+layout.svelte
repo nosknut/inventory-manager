f286f7fd6acc38cce1630ef3243a1fc91b728486
nosknut <nosknut@gmail.com>
2023-07-26 20:41:03 +0200

    Replaces default example with smui layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,53 +1,35 @@
-<script>
-	import Header from './Header.svelte';
+<script lang="ts">
+	import TopAppBar, { Row, Section, Title, AutoAdjust } from '@smui/top-app-bar';
 	import './styles.css';
+
+	let topAppBar: TopAppBar;
 </script>
 
 <div class="app">
-	<Header />
+	<TopAppBar bind:this={topAppBar} variant="standard">
+		<Row>
+			<Section>
+				<Title>Inventory Manager</Title>
+			</Section>
+		</Row>
+	</TopAppBar>
+	<AutoAdjust {topAppBar}>
+		<main>
+			<slot />
+		</main>
+	</AutoAdjust>
 
-	<main>
-		<slot />
-	</main>
-
-	<footer>
-		<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
-	</footer>
-</div>
-
-<style>
-	.app {
-		display: flex;
-		flex-direction: column;
-		min-height: 100vh;
-	}
-
-	main {
-		flex: 1;
-		display: flex;
-		flex-direction: column;
-		padding: 1rem;
-		width: 100%;
-		max-width: 64rem;
-		margin: 0 auto;
-		box-sizing: border-box;
-	}
-
-	footer {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		padding: 12px;
-	}
-
-	footer a {
-		font-weight: bold;
-	}
-
-	@media (min-width: 480px) {
-		footer {
-			padding: 12px 0;
+	<style>
+		/* Hide everything above this component. */
+		:global(#smui-app),
+		:global(body),
+		:global(html) {
+			display: block !important;
+			height: auto !important;
+			width: auto !important;
+			position: static !important;
 		}
-	}
-</style>
+	</style>
+
+	<footer />
+</div>
